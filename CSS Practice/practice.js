4976858546948ff01e61a70d36f423db8c547262
Abinesh.M <71655541+AbineshAzanth@users.noreply.github.com>
2021-11-13 13:24:39 +0530

    uploaded

---
+++ CSS Practice/practice.js
@@ -0,0 +1,13 @@
+function hellofunction(){
+    document.getElementById("SWEET").innerHTML="abinesh";
+    
+}
+function blockfunction(){
+    document.getElementById("block2d").innerHTML="abinesh";
+
+}    
+function date() {
+    document.getElementById("date").innerHTML=Date();
+    
+}
+var notes=document.setAttribute()
